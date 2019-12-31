484e4b9f5d1bd581df407fe39c9aa8e03ee33bda
Liav <liavbapp@gmail.com>
2019-12-31 22:43:40 +0200

    message obj

--- Server.py
+++ Server.py
@@ -1,4 +1,25 @@
+from
 from socket import *
+
+class HashMessage :
+    team_name = None
+    message_type = None
+    message_hash = None
+    orig_length = None
+    orig_string_start = None
+    orig_sting_end = None
+    def __init__(self, team_name, message_type, message_hash, orig_length, orig_string_start, orig_sting_end):
+        self.team_name =team_name
+        self.type = type
+        self.hash = hash
+        self.message_type = message_type
+        self.message_hash = message_hash
+        self.orig_length = orig_length
+        self.orig_string_start = orig_string_start
+        self.orig_sting_end = orig_sting_end
+
+
+
 
 def scan_range(rnage):
 
@@ -21,7 +42,6 @@ def main():
     server_socket.bind(("", server_port))
     while 1:
         message, client_address = server_socket.recvfrom(3117)
-
         server_socket.sendto(modified_message, client_address)
 
 if _name_ == '_main_':
